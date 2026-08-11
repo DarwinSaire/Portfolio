533c810e06533da9545cbeba2cabfa06e892a93e
DarwinSaire <darwinsaireonline@gmail.com>
2026-08-10 22:41:13 -0300

    updating portfolio

--- js/script.js
+++ js/script.js
@@ -20,100 +20,12 @@ window.ityped.init(document.querySelector(".iTyped"), {
     "Data Scientist",
     "NLP Developer",
     "Computer Vision Specialist",
+    "University Professor",
   ],
   loop: true,
   typeSpeed: 100,
   backSpeed: 100,
   backDelay: 1000,
-});
-
-// Portfolio Item Filter
-
-const filterContainer = document.querySelector(".portfolio-filter"),
-  filterBtns = filterContainer.children,
-  totalFilterBtn = filterBtns.length,
-  portfolioItems = document.querySelectorAll(".portfolio-item"),
-  totalPortfolioItem = portfolioItems.length;
-
-for (let i = 0; i < totalFilterBtn; i++) {
-  filterBtns[i].addEventListener("click", function () {
-    filterContainer.querySelector(".active").classList.remove("active");
-    this.classList.add("active");
-
-    const filterValue = this.getAttribute("data-filter");
-    for (let k = 0; k < totalPortfolioItem; k++) {
-      if (filterValue === portfolioItems[k].getAttribute("data-category")) {
-        portfolioItems[k].classList.remove("hide");
-        portfolioItems[k].classList.add("show");
-      } else {
-        portfolioItems[k].classList.remove("show");
-        portfolioItems[k].classList.add("hide");
-      }
-      if (filterValue === "all") {
-        portfolioItems[k].classList.remove("hide");
-        portfolioItems[k].classList.add("show");
-      }
-    }
-  });
-}
-
-// Portfolio Lighbox
-
-const lightbox = document.querySelector(".lightbox"),
-  lightboxImg = lightbox.querySelector(".lightbox-img"),
-  lightboxText = lightbox.querySelector(".caption-text"),
-  lightboxClose = lightbox.querySelector(".lightbox-close"),
-  lightboxCounter = lightbox.querySelector(".caption-counter");
-
-let itemIndex = 0;
-
-for (let i = 0; i < totalPortfolioItem; i++) {
-  portfolioItems[i].addEventListener("click", function () {
-    itemIndex = i;
-    changeItem();
-    toggleLightbox();
-  });
-}
-
-function toggleLightbox() {
-  lightbox.classList.toggle("open");
-}
-
-function changeItem() {
-  let imgSrc = portfolioItems[itemIndex]
-    .querySelector(".portfolio-img img")
-    .getAttribute("src");
-  lightboxImg.src = imgSrc;
-  lightboxText.innerHTML = portfolioItems[itemIndex].querySelector(
-    "h4"
-  ).innerHTML;
-  lightboxCounter.innerHTML = itemIndex + 1 + " of " + totalPortfolioItem;
-}
-
-function prevItem() {
-  if (itemIndex === 0) {
-    itemIndex = totalPortfolioItem - 1;
-  } else {
-    itemIndex--;
-  }
-  changeItem();
-}
-
-function nextItem() {
-  if (itemIndex === totalPortfolioItem - 1) {
-    itemIndex = 0;
-  } else {
-    itemIndex++;
-  }
-  changeItem();
-}
-
-// close lightbox
-
-lightbox.addEventListener("click", function (event) {
-  if (event.target === lightboxClose || event.target === lightbox) {
-    toggleLightbox();
-  }
 });
 
 // Aside Navbar
@@ -230,19 +142,3 @@ function latestTheme() {
 }
 // Initialize the localstorage
 latestTheme();
-
-// Lunch The Notifiaction
-  function lunchNotifiaction() {
-    // Get the snackbar DIV
-    var x = document.getElementById("notification");
-
-    // Add the "show" class to DIV
-    x.className = "show";
-
-    // After 30 seconds, remove the show class from DIV
-    setTimeout(function () {
-      x.className = x.className.replace("show", "hide");
-    }, 30000);
-  }
-// Initialize theNotification
-document.body.onload = lunchNotifiaction();
